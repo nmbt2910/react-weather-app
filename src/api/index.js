76e38d2461a54079291f98ec842e24dffc7b9a71
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2024-12-24 03:00:31 +0700

    third commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '323bb0a8e4mshd0876b49b587693p1306e7jsn50aadab22ab6';
 console.log(API_KEY);
 
 // Function to fetch the nearest place based on coordinates
